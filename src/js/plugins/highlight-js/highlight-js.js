6677a075903b89922e4bf32008337f604b4a0ef6
Ross Angus <ross.a.angus@gmail.com>
2025-03-31 21:09:20 +0100

    Added highlight.js and configured it for three languages

--- src/js/plugins/highlight-js/highlight-js.js
+++ src/js/plugins/highlight-js/highlight-js.js
@@ -1,14 +1,8 @@
-import clickEvents from "./modules/events/click-events.js";
-import mainNavSetup from "./modules/navigation/main/main-nav-setup.js";
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
 import scss from 'highlight.js/lib/languages/scss';
 import xml from 'highlight.js/lib/languages/xml';
 
-clickEvents();
-mainNavSetup();
-
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('scss', scss);
 hljs.registerLanguage('xml', xml);
-hljs.highlightAll();
