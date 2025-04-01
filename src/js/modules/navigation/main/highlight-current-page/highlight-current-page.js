b76adb39d9876758f4a0fe23852d10a93a88562b
Ross Angus <ross.a.angus@gmail.com>
2025-04-01 11:25:31 +0100

    Main navigation now expands and highlights the current page
    To do: simplify mainNavSetup() so it works as it used to

---
+++ src/js/modules/navigation/main/highlight-current-page/highlight-current-page.js
@@ -0,0 +1,50 @@
+// Recursive function which looks upward from the current DOM element
+// and attempts to find toggle buttons. It stops when it reaches
+// a `nav` element
+const toggleTree = (element) => {
+  if (element === null) return;
+
+  const toggleButton = element.querySelector(':scope > button[data-js="subnav-toggle"]');
+  // Is this element a list item?
+  // Does it have a `button` element inside?
+  if (toggleButton !== null) {
+    toggleButton.click();
+    toggleTree(element.parentElement);
+  } else if (element.tagName !== 'NAV') {
+    // Move up the tree, as long as you haven't hit the `nav` element
+    toggleTree(element.parentElement);
+  }
+};
+
+// This attempts to find the current URL within the main navigation
+// If it succeeds, it expands the navigation tree to display the link
+// and highlights it.
+// Assumptions: all links within the main navigation tree are from the
+// root of the site.
+const highlightCurrentPage = (navListItems) => {
+  if (navListItems === null) return;
+
+  // The current page
+  let path = location.pathname;
+  const lastChar = path.slice(-1);
+  // Trim off the trailing slash (if present) just in case the navigation
+  // has also done so
+  if (lastChar === "/" && path.length > 1) path = path.substring(0, path.length - 1);
+
+  // for ... of used so I can break out of the loop early
+  for (const li of navListItems) {
+    const anchor = li.querySelector('a');
+    const href = anchor.getAttribute('href');
+    // Does this element represent the current node?
+    if (href.indexOf(path) !== -1) {
+      const highlightElement = document.createElement('strong');
+      // Highlight the current node
+      li.insertBefore(highlightElement, anchor);
+      highlightElement.appendChild(anchor);
+      toggleTree(li.parentElement);
+      return false;
+    }
+  }
+};
+
+export default highlightCurrentPage;
--- src/js/modules/navigation/main/main-nav-setup.js
+++ src/js/modules/navigation/main/main-nav-setup.js
@@ -1,10 +1,9 @@
 import subnavSetup from "./sub-navigation/subnav-setup.js";
+import highlightCurrentPage from "./highlight-current-page/highlight-current-page.js";
 
-// This runs once to set up all the extra markup which the main navigation needs
-// in order to work nicely
-const mainNavSetup = () => {
+const callSubnavSetup = (navUl) => {
   // This is just for nested navigation
-  const navElements = document.querySelectorAll('header nav li');
+  const navElements = navUl.querySelectorAll('li');
   if (navElements.length > 0) {
     navElements.forEach((navLi, index) => {
       // Does this li have both an anchor tag and a nested `ul`?
@@ -13,7 +12,19 @@ const mainNavSetup = () => {
       // Does this node have the required markup?
       anchor !== null && ul !== null && subnavSetup(anchor, ul, index);
     });
+
+    // Call highlight
+    highlightCurrentPage(navElements);
   }
+};
+
+// This runs once to set up all the extra markup which the main navigation needs
+// in order to work nicely
+const mainNavSetup = () => {
+  const navUls = document.querySelectorAll('header nav > ul');
+  navUls.forEach(navUl => {
+    callSubnavSetup(navUl);
+  });
 };
 
 export default mainNavSetup;
