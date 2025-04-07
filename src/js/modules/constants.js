757d0a76418d68902dca72b2725fe5425bbfc9d7
Ross Angus <ross.a.angus@gmail.com>
2025-04-07 13:15:54 +0100

    Fixed constants typo

--- src/js/modules/constants.js
+++ src/js/modules/constants.js
@@ -1,6 +1,6 @@
 export default {
   OPEN_MAINNAV_STRING: 'Open main navigation',
-  CLOSE_MANINAV_STRING: 'Close main navigation',
+  CLOSE_MAINNAV_STRING: 'Close main navigation',
   OPEN_SUBNAV_STRING: 'Open sub-navigation',
   CLOSE_SUBNAV_STRING: 'Close sub-navigation',
 }
