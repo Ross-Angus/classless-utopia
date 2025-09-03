212d477c2955e81c4680d94014d52f3e59faa73d
Ross Angus <Ross-Angus@users.noreply.github.com>
2025-09-03 14:17:53 +0100

    The prepare task now builds out the hero images too

--- tools/compress-all-images.js
+++ tools/compress-all-images.js
@@ -1,9 +1,18 @@
 import getFiles from "./get-files.js";
 import writeImages from "./write-images.js";
+import hero from "./image-scripts/hero.js";
 
-const allImagePaths = getFiles('./src/img');
+const allImagePaths = getFiles("./src/img");
 
 allImagePaths.map((path) => {
-  const trimPath = path.replace('./', '');
-  writeImages(trimPath);
+  const trimPath = path.replace("./", "");
+  const checkPath = trimPath.replaceAll("\\", "/");
+
+  // Each special handling script uses a different path to trigger it
+  if (checkPath.includes("src/img/hero/")) {
+    // Hero images special case
+    hero(checkPath);
+  } else {
+    writeImages(trimPath);
+  }
 });
--- tools/image-compress.js
+++ tools/image-compress.js
@@ -5,14 +5,14 @@ import hero from "./image-scripts/hero.js";
 // Destructuring the Array from Node which includes data we need
 const [node, thisFile, filePath, fileEvent] = argv;
 // White-list of events which should cause Sharp to generate images
-const triggerEvents = ['add', 'change'];
+const triggerEvents = ["add", "change"];
 
 // If the wrong kind of event triggers this script, do nothing
 if (triggerEvents.includes(fileEvent)) {
-  const checkPath = filePath.replaceAll('\\', '/');
+  const checkPath = filePath.replaceAll("\\", "/");
 
   // Each special handling script uses a different path to trigger it
-  if (checkPath.includes('src/img/hero/')) {
+  if (checkPath.includes("src/img/hero/")) {
     // Hero images special case
     hero(checkPath);
   } else {
